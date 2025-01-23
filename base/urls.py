98063035f6f71a70f82c7229794c5a79d438972b
PolsaFH <filiphj01@gmail.com>
2025-01-23 01:07:01 +0100

    Login implemented
    Login page is without styling so need to fix that soon. Elias could you make the design and style the page.

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+
     path('', views.home, name='home'),
     path('schematic/show/<str:pk>/', views.schematic, name='schematic'),
     path('schematic/upload/', views.upload, name='upload'),
--- base/views.py
+++ base/views.py
@@ -1,10 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 schems = [
     {'id': 1, 'name': 'Schematic 1'},
     {'id': 2, 'name': 'Schematic 2'},
     {'id': 3, 'name': 'Erik er søt'},
 ]
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username is incorrect')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            nextPage = request.GET.get('next')
+            if nextPage:
+                return redirect(nextPage)
+            else:
+                return redirect('home')
+        else:
+            messages.error(request, 'Password is incorrect')
+    
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 # Create your views here.
 def home(request):
@@ -22,5 +59,6 @@ def schematic(request, pk):
     context = {'schem': schem}
     return render(request, 'base/schematic.html', context)
 
+@login_required(login_url='login')
 def upload(request):
     return render(request, 'base/upload.html')
